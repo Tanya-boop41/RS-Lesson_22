862e48bc2870788806a36ccaa83ac5dd35a6230f
Tanya <efita41@gmail.com>
2024-11-13 15:25:45 +0300

    add form contacts, record erb

--- app.rb
+++ app.rb
@@ -18,3 +18,19 @@ end
 get '/contacts' do
 	erb :contacts
 end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@date_time = params[:date_time]
+
+	f = File.open './public/users.txt', 'a'
+	f.write "Имя: #{@username}, телефон: #{@phone}, дата и время записи: #{@date_time} \n"
+	f.close
+
+	erb :record
+end
+
+post '/record' do
+	erb :visit
+end
